f68c83703afeb0f0c82a17ef04632414f8fbb8cf
Eric Tao <etao9039@gmail.com>
2025-01-25 02:10:43 -0800

    Add initial implementation of chat model using Google Generative AI

---
+++ backend/main.py
@@ -0,0 +1,42 @@
+import os
+import google.generativeai as genai
+
+os.environ["GEMINI_API_KEY"]
+
+genai.configure(api_key=os.environ["GEMINI_API_KEY"])
+
+# Create the model
+generation_config = {
+  "temperature": 1,
+  "top_p": 0.95,
+  "top_k": 40,
+  "max_output_tokens": 8192,
+  "response_mime_type": "text/plain",
+}
+
+model = genai.GenerativeModel(
+  model_name="gemini-1.5-flash",
+  generation_config=generation_config,
+)
+
+chat_session = model.start_chat(
+  history=[
+      {
+      "role": "user",
+      "parts": [
+        "",
+      ],
+    },
+    {
+      "role": "model",
+      "parts": [
+        "",
+      ],
+    },
+      
+  ]
+)
+
+response = chat_session.send_message("")
+
+print(response.text)
